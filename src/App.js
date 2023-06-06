b2d17ab9dc97d781d4d470ef950302688b51209c
pRemant <paudelremant45@gmail.com>
2023-06-06 23:25:00 +0545

    document title added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useEffect } from 'react'
 import RegForm from './component/RegForm'
 const App = () => {
+  useEffect(() => {
+    document.title = "RegForm";
+  }, []);
   return (
     <>
       <RegForm/>
